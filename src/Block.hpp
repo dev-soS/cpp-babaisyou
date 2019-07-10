c735888c432bf21945c86adb09a0976a75cbcdc3
unknown <jongmeoung8520@gmail.com>
2019-07-10 19:43:42 +0900

    Block: add property and define block class

--- src/Block.hpp
+++ src/Block.hpp
@@ -1,14 +1,36 @@
+#ifndef BLOCK_HPP
+#define BLOCK_HPP
+
+#include "Property.hpp"
+#include <vector>
+
 class Block
 {
-
+protected :
+	std::vector<Property> properties;
+	std::string block_name;
+public :
+	Block();
 };
 
 class Text : public Block
 {
-	static Text 
+private :
+
+public :
+	static Text baba;
+	static Text is;
+	static Text you;
+	static Text flag;
+	static Text win;
+	static Text push;
+	static Text stop;
 };
 
 class Entity : public Block
 {
-
+	static Entity baba;
+	static Entity flag;
 };
+
+#endif
