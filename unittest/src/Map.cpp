386a0c22f01aabc733cf3cbaf55bc2ff182199d9
Shin <tlsdntmd98@gmail.com>
2019-08-01 16:44:12 +0900

    map: Add default ctor test

---
+++ unittest/src/Map.cpp
@@ -0,0 +1,12 @@
+#include "Catch2/catch.hpp"
+
+#include "Map.hpp"
+
+TEST_CASE("Default Constructor", "[Map]") {
+    Map<10, 10> map;
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            REQUIRE(map[i][j] == nullptr);
+        }
+    }
+}
