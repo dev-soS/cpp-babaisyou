3e8c7967f200982cff7c8aaf3f9eaf200ca6cbb8
revsic <revsic99@gmail.com>
2019-08-18 00:27:31 +0900

    map wrapper: Add update blocks test

--- unittest/src/MapWrapper.cpp
+++ unittest/src/MapWrapper.cpp
@@ -139,5 +139,84 @@ TEST_CASE("MapWrapper::move", "[MapWrapper]")
 
 TEST_CASE("MapWrapper::updateBlocks", "[MapWrapper]")
 {
+    using PosVec = std::vector<std::tuple<size_t, size_t>>;
 
+    // map nullity validation
+    MapWrapper<4, 3> empty_wrapper;
+    REQUIRE(!empty_wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+
+    // preparing test
+    Map<4, 3> map;
+    MapWrapper wrapper(&map);
+
+    const char* visual = "123456789";
+
+    Entity baba_entity(BlockId::BABA, {}, visual);
+    Text baba_text(BlockId::BABA, Property::ENTITY, {}, visual, &baba_entity);
+
+    Entity flag_entity(BlockId::FLAG, {}, visual);
+    Text flag_text(BlockId::FLAG, Property::ENTITY, {}, visual, &flag_entity);
+
+    Text is_text(BlockId::IS, Property::INVALID, {}, visual);
+    Text you_text(BlockId::YOU, Property::YOU, {}, visual);
+
+    // `IS` text validation
+    // 0. block nullity validation
+    REQUIRE(!wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+
+    // 1. coordinate validation
+    map[0][2] = &you_text;
+    map[0][3] = &you_text;
+    REQUIRE(!wrapper.updateBlocks(std::make_tuple(2, 0), MoveType::RIGHT, 3));
+
+    // 2. no `IS` text
+    map[0][1] = &you_text;
+    REQUIRE(!wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+
+    // 3. first block `IS`
+    map[0][1] = &is_text;
+    REQUIRE(!wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+
+    // 4. last block `IS`
+    map[0][1] = &you_text;
+    map[0][2] = &is_text;
+    REQUIRE(!wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+
+    // adjacenct text validation
+    map[0][1] = &baba_entity;
+    map[0][2] = &is_text;
+    map[0][3] = &you_text;
+    REQUIRE(!wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+
+    map[0][3] = &flag_entity;
+    REQUIRE(!wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+
+    map[0][1] = &baba_text;
+    REQUIRE(!wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+
+    // left property
+    map[0][1] = &you_text;
+    map[0][3] = &baba_text;
+    REQUIRE(!wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+
+    // left entity, right property
+    map[0][1] = &baba_text;
+    map[0][3] = &you_text;
+    REQUIRE(wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+    REQUIRE(baba_entity.containProperty(Property::YOU));
+
+    // left entity, right entity
+    map[0][1] = &baba_text;
+    map[0][3] = &flag_text;
+
+    map[1][1] = &baba_entity;
+    map[1][3] = &flag_entity;
+    baba_entity.addPosition(std::make_tuple(1, 1));
+    flag_entity.addPosition(std::make_tuple(3, 1));
+
+    REQUIRE(wrapper.updateBlocks(std::make_tuple(1, 0), MoveType::RIGHT, 3));
+    REQUIRE(baba_entity.getPosition() == PosVec{});
+    REQUIRE(flag_entity.getPosition() == PosVec{ std::make_tuple(3, 1), std::make_tuple(1, 1) });
+    REQUIRE(map[1][1] == &flag_entity);
+    REQUIRE(map[1][3] == &flag_entity);
 }
