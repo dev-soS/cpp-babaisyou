9977858aa5857cce76d8764fb4cd6cf7c2c8ed9f
Shin <tlsdntmd98@gmail.com>
2019-08-18 02:30:21 +0900

    Map: Add test case

--- unittest/src/Map.cpp
+++ unittest/src/Map.cpp
@@ -13,3 +13,35 @@ TEST_CASE("Default Constructor", "[Map]")
         }
     }
 }
+
+TEST_CASE("[]Operator Overloading","[Map]")
+{
+    Block block1(BlockId::BABA, BlockType::ENTITY, { Property::PUSH }, "         ");
+    Block block2(BlockId::BABA, BlockType::ENTITY, { Property::YOU }, "         ");
+    Block block3(BlockId::BABA, BlockType::ENTITY, { Property::WIN }, "         ");
+    Block block4(BlockId::BABA, BlockType::ENTITY, { Property::STOP }, "         ");
+
+    Map<2,2> map;
+
+    map[0][0] = &block1;
+    map[0][1] = &block2;
+    map[1][0] = &block3;
+    map[1][1] = &block4;
+
+    REQUIRE(map[0][0] == &block1 );
+    REQUIRE(map[0][1] == &block2 );
+    REQUIRE(map[1][0] == &block3 );
+    REQUIRE(map[1][1] == &block4 );
+}
+
+TEST_CASE("getWidth","[Map]")
+{
+    Map<10,10> map;
+    REQUIRE(map.getWidth == 10);
+}
+
+TEST_CASE("getHeight","[Map]")
+{
+    Map<10,10> map;
+    REQUIRE(map.getHeight == 10);
+}
