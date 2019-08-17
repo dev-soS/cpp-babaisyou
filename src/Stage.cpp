3cacff0a217d82987460d57b8dcab700cf21581d
unknown <jongmeoung8520@gmail.com>
2019-08-17 22:38:47 +0900

    Stage: add print func simply

--- src/Stage.cpp
+++ src/Stage.cpp
@@ -1,7 +1,7 @@
 #include "Stage.hpp"
 
 Stage::Stage(StageName stage_name) 
-	: stage(Map<20, 10>()), stage_wrapper(MapWrapper<20, 10>(stage))
+	: stage(Map<20, 10>())
 {
 	switch ( stage_name )
 	{
@@ -17,10 +17,40 @@ Stage::Stage(StageName stage_name)
 		stage[5][2] = &Entity::baba;
 		stage[5][17] = &Entity::flag;
 
-		
+		stage.update();
 		break;
 	case StageName::STAGE1_2:
 
 		break;
+	}
+}
+
+void Stage::gotoxy(int x, int y)
+{
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void Stage::printBlock(Block* block, int x, int y) const
+{
+	for ( int i = 0; i < 3; ++i )
+	{
+		gotoxy(3 * x, 3 * y + i);
+		for ( int j = 0; j < 3; ++j )
+		{
+			std::cout << block->getBlockVisual()[i][j];
+		}
+	}
+}
+
+
+void Stage::printMapOverall() const
+{
+	for (int i = 0; i < 10; ++i )
+	{
+		for ( j = 0; j < 20; ++j )
+		{
+			printBlock(stage[i][j], j, i);
+		}
 	}
 }
--- src/Stage.hpp
+++ src/Stage.hpp
@@ -5,12 +5,16 @@
 #include "Map.hpp"
 #include "MapWarpper.hpp"
 
+#include <Windows.h>
+
 class Stage
 {
 private:
 	Map<20, 10> stage;
-	MapWrapper<20, 10> stage_wrapper;
 public:
 	Stage(StageName stage_name);
+	void gotoxy(int x, int y);
+	void printBlock(Block* block, int x, int y) const;
+	void printMapOverall() const;
 };
 #endif
