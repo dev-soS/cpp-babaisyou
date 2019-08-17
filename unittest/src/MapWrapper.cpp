41086c78f31ba82c6480292b1eae22964f5390d7
revsic <revsic99@gmail.com>
2019-08-17 22:54:29 +0900

    map wrapper: Add move test

--- unittest/src/MapWrapper.cpp
+++ unittest/src/MapWrapper.cpp
@@ -75,7 +75,66 @@ TEST_CASE("MapWrapper::movable", "[MapWrapper]")
 
 TEST_CASE("MapWrapper::move", "[MapWrapper]")
 {
+    using PosVec = std::vector<std::tuple<size_t, size_t>>;
 
+    // empty map validation
+    MapWrapper<4, 3> empty_wrapper;
+    REQUIRE(!empty_wrapper.move(std::make_tuple(1, 0), MoveType::RIGHT));
+
+    // preparing test
+    Map<4, 3> map;
+    MapWrapper wrapper(&map);
+
+    // movable entity validation
+    // 1. entity nullity
+    REQUIRE(!wrapper.move(std::make_tuple(1, 0), MoveType::RIGHT));
+
+    Entity entity(BlockId::BABA, {}, "123456789");
+    entity.addPosition(std::make_tuple(1, 0));
+    map[0][1] = &entity;
+
+    // 2. push validation
+    REQUIRE(!wrapper.move(std::make_tuple(1, 0), MoveType::RIGHT));
+    REQUIRE(map[0][1] == &entity);
+    REQUIRE(entity.getPosition() == PosVec{ std::make_tuple(1, 0) });
+
+    entity.addProperty(Property::PUSH);
+    REQUIRE(wrapper.move(std::make_tuple(1, 0), MoveType::RIGHT));
+    REQUIRE(map[0][2] == &entity);
+    REQUIRE(entity.getPosition() == PosVec{ std::make_tuple(2, 0) });
+
+    // 3. coordinate validation
+    REQUIRE(!wrapper.move(std::make_tuple(2, 0), MoveType::UP));
+    REQUIRE(map[0][2] == &entity);
+    REQUIRE(entity.getPosition() == PosVec{ std::make_tuple(2, 0) });
+
+    // 4. stop validation
+    Entity entity2(BlockId::FLAG, {}, "123456789");
+    map[0][3] = &entity2;
+
+    REQUIRE(wrapper.move(std::make_tuple(2, 0), MoveType::RIGHT));
+    REQUIRE(map[0][3] == &entity);
+    REQUIRE(entity.getPosition() == PosVec{ std::make_tuple(3, 0) });
+
+    entity2.addProperty(Property::STOP);
+    entity2.addPosition(std::make_tuple(2, 0));
+    map[0][2] = &entity2;
+
+    REQUIRE(!wrapper.move(std::make_tuple(3, 0), MoveType::LEFT));
+    REQUIRE(map[0][2] == &entity2);
+    REQUIRE(entity2.getPosition() == PosVec{ std::make_tuple(2, 0) });
+    REQUIRE(map[0][3] == &entity);
+    REQUIRE(entity.getPosition() == PosVec{ std::make_tuple(3, 0) });
+
+    // adjacent entity validation
+    entity2.addProperty(Property::PUSH);
+    entity2.removeProperty(Property::STOP);
+
+    REQUIRE(wrapper.move(std::make_tuple(3, 0), MoveType::LEFT));
+    REQUIRE(map[0][2] == &entity);
+    REQUIRE(entity.getPosition() == PosVec{ std::make_tuple(2, 0) });
+    REQUIRE(map[0][1] == &entity2);
+    REQUIRE(entity2.getPosition() == PosVec{ std::make_tuple(1, 0) });
 }
 
 TEST_CASE("MapWrapper::updateBlocks", "[MapWrapper]")
