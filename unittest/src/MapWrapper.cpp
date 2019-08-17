73f74b0ca1282fcf1b38c8e0b84f15ad1ef5cdb4
revsic <revsic99@gmail.com>
2019-08-17 21:15:25 +0900

    map wrapper: Add movable test

--- unittest/src/MapWrapper.cpp
+++ unittest/src/MapWrapper.cpp
@@ -15,30 +15,56 @@ TEST_CASE("MapWrapper::Constructor", "[MapWrapper]")
 
 TEST_CASE("MapWrapper::next", "[MapWrapper]")
 {
+    auto fail = [](auto tup) { return !std::get<0>(tup); };
+
     REQUIRE(MapWrapper<3, 4>::next(std::make_tuple(0, 0), MoveType::RIGHT)
         == std::make_tuple(true, 1, 0));
-    auto[fail, x, y] = MapWrapper<3, 4>::next(std::make_tuple(0, 0), MoveType::LEFT);
-    REQUIRE(!fail);
+    REQUIRE(fail(MapWrapper<3, 4>::next(std::make_tuple(0, 0), MoveType::LEFT)));
 
-    auto[fail1, x1, y1] = MapWrapper<3, 4>::next(std::make_tuple(2, 0), MoveType::RIGHT);
-    REQUIRE(!fail1);
+    REQUIRE(fail(MapWrapper<3, 4>::next(std::make_tuple(2, 0), MoveType::RIGHT)));
     REQUIRE(MapWrapper<3, 4>::next(std::make_tuple(2, 0), MoveType::LEFT)
         == std::make_tuple(true, 1, 0));
     
-    auto[fail2, x2, y2] = MapWrapper<3, 4>::next(std::make_tuple(2, 0), MoveType::UP);
-    REQUIRE(!fail2);
+    REQUIRE(fail(MapWrapper<3, 4>::next(std::make_tuple(2, 0), MoveType::UP)));
     REQUIRE(MapWrapper<3, 4>::next(std::make_tuple(2, 0), MoveType::DOWN)
         == std::make_tuple(true, 2, 1));
     
     REQUIRE(MapWrapper<3, 4>::next(std::make_tuple(2, 3), MoveType::UP)
         == std::make_tuple(true, 2, 2));
-    auto[fail3, x3, y3] = MapWrapper<3, 4>::next(std::make_tuple(2, 3), MoveType::DOWN);
-    REQUIRE(!fail3);
+    REQUIRE(fail(MapWrapper<3, 4>::next(std::make_tuple(2, 3), MoveType::DOWN)));
 }
 
 TEST_CASE("MapWrapper::movable", "[MapWrapper]")
 {
+    auto fail = [](auto tup) { return !std::get<0>(tup); };
 
+    // empty map validation
+    MapWrapper<3, 4> empty_wrapper;
+    REQUIRE(fail(empty_wrapper.movable(std::make_tuple(0, 0), MoveType::LEFT)));
+
+    // preparing test
+    Map<3, 4> map;
+    MapWrapper wrapper(&map);
+
+    Entity entity(BlockId::BABA, {}, "123456789");
+    map[1][1] = &entity;
+
+    // push validation
+    REQUIRE(fail(wrapper.movable(std::make_tuple(1, 1), MoveType::RIGHT)));
+
+    entity.addProperty(Property::PUSH);
+    REQUIRE(wrapper.movable(std::make_tuple(1, 1), MoveType::RIGHT)
+        == std::make_tuple(true, 2, 1));
+
+    // stop validation
+    Entity entity2(BlockId::FLAG, {}, "123456789");
+    map[1][2] = &entity2;
+
+    REQUIRE(wrapper.movable(std::make_tuple(1, 1), MoveType::RIGHT)
+        == std::make_tuple(true, 2, 1));
+
+    entity2.addProperty(Property::STOP);
+    REQUIRE(fail(wrapper.movable(std::make_tuple(1, 1), MoveType::RIGHT)));
 }
 
 TEST_CASE("MapWrapper::move", "[MapWrapper]")
