14a2b76e3ca1c912b9a3f8552bb9108c33222b4d
revsic <revsic99@gmail.com>
2019-08-23 01:38:29 +0900

    gameagent: Add IOInterface based ctor

--- src/GameAgent.hpp
+++ src/GameAgent.hpp
@@ -2,15 +2,25 @@
 #define GAMEAGENT_HPP
 
 #include "Block.hpp"
+#include "IOInterface.hpp"
 #include "Map.hpp"
 
 class GameAgent
 {
 public:
-    GameAgent() = default;
+    GameAgent(IOInterface* io);
     ~GameAgent() = default;
+
+    template <size_t Width, size_t Height>
+    void Run(const Map<Width, Height>& map);
 
     bool checkWinCondition(const std::vector<Entity::Position>& you, const std::vector<Entity::Position>& win);
     bool checkRemainYou(const std::vector<Entity::Position>& you);
+
+private:
+    IOInterface* io;
+
+    template <size_t Width, size_t Height>
+    Map<Width, Height> CopyMap();
 };
 #endif
